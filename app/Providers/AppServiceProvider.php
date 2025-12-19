c82f9d86bae53ca93b086cc6b8a306e41a253535
fyy <66344025+sugmamewingss@users.noreply.github.com>
2025-12-19 16:02:49 +0700

    Ensure compiled views directory exists
    Create the compiled views directory if it doesn't exist.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if (!is_dir(config('view.compiled'))) {
+        mkdir(config('view.compiled'), 0755, true);
+    }
+        
         Gate::policy(Event::class, EventPolicy::class);
 
         Gate::define('manage-users', function ($user) {
